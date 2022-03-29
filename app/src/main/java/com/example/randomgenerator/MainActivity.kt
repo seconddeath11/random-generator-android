951af4171adfc91cc127ecded3d8b8590516b683
Diana Novikova <novikova_diana2000@mail.ru>
2022-03-29 18:58:10 +0300

    full

--- app/src/main/java/com/example/randomgenerator/MainActivity.kt
+++ app/src/main/java/com/example/randomgenerator/MainActivity.kt
@@ -5,25 +5,31 @@ import androidx.databinding.DataBindingUtil
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.randomgenerator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    //private lateinit var drawerLayout: DrawerLayout
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         val navController = this.findNavController(R.id.navHostFragment)
-        //drawerLayout = binding.drawerLayout
-
+        drawerLayout = binding.drawerLayout
+        NavigationUI.setupActionBarWithNavController(this, navController)
         NavigationUI.setupWithNavController(binding.navView, navController)
-        //NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+    }
 
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.navHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 
 
